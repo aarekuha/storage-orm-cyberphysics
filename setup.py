cb0157b69efb29d03ca1f66ad749955ef5f601b8
rekukha.aa <aleksandr.rekukha@domru.ru>
2022-11-21 20:12:41 +0500

    dev: version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 :copyright: (c) 2022 aarekuha
 """
 
-version = '1.1.1'
+version = '1.1.2'
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
